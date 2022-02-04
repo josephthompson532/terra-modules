49a241a5435b09abf6624fc3a1c92ef0f0a1bba8
Joe Thompson <josephthompson532@gmail.com>
2022-02-03 21:42:45 -0800

    guh

--- modules/data-storage/dynamodb/main.tf
+++ modules/data-storage/dynamodb/main.tf
@@ -2,7 +2,7 @@ module "dynamodb-table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
   version = "1.2.2"
 
-  name              = var.name
+  name              = var.table
   hash_key          = var.hash_key
 
   attributes        = var.attributes
